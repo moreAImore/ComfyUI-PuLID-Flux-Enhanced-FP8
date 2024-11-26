13f2e3ea22456fac99565611dba3e4fbbe5497df
moreAImore <aitopai@outlook.com>
2024-11-26 13:51:33 +0100

    Update openai.py
    FP8

--- eva_clip/openai.py
+++ eva_clip/openai.py
@@ -12,6 +12,16 @@ import torch
 from .model import build_model_from_openai_state_dict, convert_weights_to_lp, get_cast_dtype
 from .pretrained import get_pretrained_url, list_pretrained_models_by_tag, download_pretrained_from_url
 
+######-------------------------------------
+import json
+try:
+    from optimum.quanto import requantize
+except ImportError:
+    requantize = None
+    print("Please install 'optimum-quanto' for FP8 quantization support.")
+######-------------------------------------
+
+
 __all__ = ["list_openai_models", "load_openai_model"]
 
 
@@ -20,13 +30,24 @@ def list_openai_models() -> List[str]:
     return list_pretrained_models_by_tag('openai')
 
 
+# def load_openai_model(
+        # name: str,
+        # precision: Optional[str] = None,
+        # device: Optional[Union[str, torch.device]] = None,
+        # jit: bool = True,
+        # cache_dir: Optional[str] = None,
+# ):
+######-------------------------------------
 def load_openai_model(
         name: str,
         precision: Optional[str] = None,
         device: Optional[Union[str, torch.device]] = None,
         jit: bool = True,
         cache_dir: Optional[str] = None,
+        fp8: bool = False,
+        quantization_map_path: Optional[str] = None,
 ):
+######-------------------------------------
     """Load a CLIP model
 
     Parameters
@@ -75,19 +96,47 @@ def load_openai_model(
     if not jit:
         # Build a non-jit model from the OpenAI jitted model state dict
         cast_dtype = get_cast_dtype(precision)
+        # try:
+            # model = build_model_from_openai_state_dict(state_dict or model.state_dict(), cast_dtype=cast_dtype)
+        # except KeyError:
+            # sd = {k[7:]: v for k, v in state_dict["state_dict"].items()}
+            # model = build_model_from_openai_state_dict(sd, cast_dtype=cast_dtype)
+        ######-------------------------------------    
         try:
-            model = build_model_from_openai_state_dict(state_dict or model.state_dict(), cast_dtype=cast_dtype)
+            state_dict = state_dict or model.state_dict()
+            model = build_model_from_openai_state_dict(
+                state_dict,
+                cast_dtype=cast_dtype,
+                fp8=fp8,
+                quantization_map_path=quantization_map_path,
+                device=device,
+            )
         except KeyError:
             sd = {k[7:]: v for k, v in state_dict["state_dict"].items()}
-            model = build_model_from_openai_state_dict(sd, cast_dtype=cast_dtype)
-
+            model = build_model_from_openai_state_dict(
+                sd,
+                cast_dtype=cast_dtype,
+                fp8=fp8,
+                quantization_map_path=quantization_map_path,
+                device=device,
+            )
+        ######-------------------------------------
+        
         # model from OpenAI state dict is in manually cast fp16 mode, must be converted for AMP/fp32/bf16 use
         model = model.to(device)
+        # if precision.startswith('amp') or precision == 'fp32':
+            # model.float()
+        # elif precision == 'bf16':
+            # convert_weights_to_lp(model, dtype=torch.bfloat16)
+        # if not fp8:
+        ######-------------------------------------
+        # Convert model to desired precision if not using FP8
         if precision.startswith('amp') or precision == 'fp32':
             model.float()
         elif precision == 'bf16':
             convert_weights_to_lp(model, dtype=torch.bfloat16)
-
+        ######-------------------------------------
+        
         return model
 
     # patch the device names
